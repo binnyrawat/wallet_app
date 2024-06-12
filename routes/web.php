d4140510732b262e16e7adcdc3184719afc03ddf
Sunil <rawat8457@gmail.com>
2024-06-12 21:04:05 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('cache_clear',function(){
+    \Artisan::call('key:generate');
+});
+
 Route::get('sign-up',[AuthController::class,'signUp'])->name('sign-up');
 Route::post('register-user',[AuthController::class,'registerUser'])->name('register-user');
 Route::get('login',[AuthController::class,'login'])->name('login');
