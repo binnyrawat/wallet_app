754c07560e1134f817bdc2aa723203c52f7f786d
Sunil <rawat8457@gmail.com>
2024-06-12 21:11:40 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,3 +15,7 @@ Route::get('sign-up',[AuthController::class,'signUp'])->name('sign-up');
 Route::post('register-user',[AuthController::class,'registerUser'])->name('register-user');
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::post('login',[AuthController::class,'checkLogin'])->name('checkLogin');
+Route::group(['prefix'=>'user','middleware'=>'auth'],function(){
+    Route::get('dashboard',[DashboardController::class,'dashboard']);
+});
+
